abbc164740d26456f1ca7b5a61f45a7697654fab
Amadou CISSE <105439965+1Amadou@users.noreply.github.com>
2023-08-15 13:32:01 +0000

    Update task_spec.rb

--- spec/models/task_spec.rb
+++ spec/models/task_spec.rb
@@ -2,16 +2,39 @@ require 'rails_helper'
 
 RSpec.describe Task, type: :model do
   describe "Validation" do
-    it "Si aucun Title n'est saisi, la Validation de la tâche est invalide." do
+    it "adds an error when Title is not provided" do
       task = Task.new(title: nil, description: "test", status: :todo, deadline: Time.current)
-      expect(task).to be_invalid
-      expect(task.errors.full_messages).to eq ["Title can't be blank"]
+      task.valid?
+      expect(task.errors[:title]).to include("can't be blank")
     end
 
-    it "Si aucun statut n'est saisi, la Validation de la tâche n'est pas valide." do
+    it "adds an error when Status is not provided" do
       task = Task.new(title: "test", description: "test", status: nil, deadline: Time.current)
-      expect(task).to be_invalid
-      expect(task.errors.full_messages).to eq ["Status can't be blank"]
+      task.valid?
+      expect(task.errors[:status]).to include("can't be blank")
+    end
+
+    it "adds an error when Deadline is not provided" do
+      task = Task.new(title: "test", description: "test", status: :todo, deadline: nil)
+      task.valid?
+      expect(task.errors[:deadline]).to include("can't be blank")
+    end
+
+    it "adds an error when Deadline is in the past" do
+      task = Task.new(title: "test", description: "test", status: :todo, deadline: 1.day.ago)
+      task.valid?
+      expect(task.errors[:deadline]).to include("must start from today.")
+    end
+
+    it "does not add errors when all attributes are valid" do
+      task = Task.new(title: "test", description: "test", status: :todo, deadline: Time.current)
+      expect(task).to be_valid
+    end
+
+    it "adds an error when Deadline is in the past" do
+      task = Task.new(title: "test", description: "test", status: :todo, deadline: 1.day.ago)
+      task.valid?
+      expect(task.errors[:deadline]).to include("must start from today.")
     end
   end
 end
