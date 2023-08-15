89a546c4d577c50e06458bf824cae4c9236a3ec9
Amadou CISSE <105439965+1Amadou@users.noreply.github.com>
2023-08-15 12:15:31 +0000

    Update task_spec.rb

--- spec/models/task_spec.rb
+++ spec/models/task_spec.rb
@@ -1,5 +1,17 @@
 require 'rails_helper'
 
 RSpec.describe Task, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "Validation" do
+    it "Si aucun Title n'est saisi, la Validation de la tâche est invalide." do
+      task = Task.new(title: nil, description: "test", status: :todo, deadline: Time.current)
+      expect(task).to be_invalid
+      expect(task.errors.full_messages).to eq ["Title can't be blank"]
+    end
+
+    it "Si aucun statut n'est saisi, la Validation de la tâche n'est pas valide." do
+      task = Task.new(title: "test", description: "test", status: nil, deadline: Time.current)
+      expect(task).to be_invalid
+      expect(task.errors.full_messages).to eq ["Status can't be blank"]
+    end
+  end
 end
